8ac3bf7c56548e935a79cfc79cb241d6c1b38c23
yufen <yufenhuangtaiwan@gmail.com>
2025-01-19 23:49:40 +0800

    feat: integrate getWelcomeMessage with Application

--- src/main/java/Yuuufen/runnerz/Application.java
+++ src/main/java/Yuuufen/runnerz/Application.java
@@ -2,12 +2,20 @@ package Yuuufen.runnerz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+		// Retrieve WelcomeMessage bean from Spring context
+		WelcomeMessage welcomeMessage = (WelcomeMessage) context.getBean("welcomeMessage");
+
+		// Call getWelcomeMessage() method and print its return value
+		System.out.println(welcomeMessage.getWelcomeMessage());
+
 	}
 
 }
